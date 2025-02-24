8dcbf6d4282faba9ec1886b788ca64a2d425b56a
hary077 <mhary9115@gmail.com>
2025-02-24 22:03:49 +0700

    menambahkan config

--- config.php
+++ config.php
@@ -1,1 +1,20 @@
-mmmmm
+<?php
+
+//konfigurasi koneksi database
+$host = "localhost";//nama host server database
+$username = "root";//username untuk akses ke database
+$password ="";//pasword untuk akses ke databse
+$database = "event_invitation";//nama database yg digunakan
+
+// membuat koneksi ke database mengguanakan databse mysqli
+$conn = mysqli_connect($host,$username,$password,$database);
+
+// mengecek apakah koneksi berhasil
+if ($conn->connect_error){
+    //menampilkan pesan eror jika koneksi gagal
+    die ("database gagal terkoneksi: ". $conn->connect_error);
+
+    //jika koneksi berhasil script akan jalan tanpa pesan eror
+
+}
+?>
