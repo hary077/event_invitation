f50f319a7d474a947e2112d4ba877b79007d75a2
hary077 <mhary9115@gmail.com>
2025-02-24 22:16:56 +0700

    memperbaiki nama

--- auth/login.php
+++ auth/login.php
@@ -5,11 +5,11 @@
     <!-- Logo -->
     <div class="app-brand justify-content-center">
       <a href="index.html" class="app-brand-link gap-2">
-        <span class="app-brand-text demo text-uppercase fw-bolder">event_invitation</span>
+        <span class="app-brand-text demo text-uppercase fw-bolder">event invitation</span>
       </a>
     </div>
     <!-- /Logo -->
-    <h4 class="mb-2">Selamat datang di event_invitation! 👋</h4>
+    <h4 class="mb-2">Selamat datang di event invitation! 👋</h4>
     <form class="mb-3" action="login_auth.php" method="POST">
       <div class="mb-3">
         <label class="form-label">Username</label>
