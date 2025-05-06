bddd5927f2bcef9656093583af15311a1cfcedcb
hary077 <mhary9115@gmail.com>
2025-05-06 07:32:03 +0700

    mengganti ico

--- auth/.layouts/header.php
+++ auth/.layouts/header.php
@@ -3,10 +3,10 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-    <title>IdeKreatif</title>
+    <title>Even Invitation</title>
     <meta name="description" content="" />
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="../assets/img/favicon/undangan.ico" />
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"/>
     <!-- Icons. Uncomment required icon fonts -->
